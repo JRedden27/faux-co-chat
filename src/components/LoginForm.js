5d1ed2ac0ab2a32af0a683e60a3197f5a033f723
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-09-01 22:08:19 -0400

    more edits

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-import Logo from "../assets/FauxCoChat.png";
+import Logo from "../assets/FauxCoChat4b.png";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
